9ce1d1d8185a79026f3c1869233449966b5357da
Travis Johnson <53201762+travisdjohnson@users.noreply.github.com>
2021-07-16 08:05:08 -0700

    Create Time.py

---
+++ Time.py
@@ -0,0 +1,6 @@
+import datetime
+CA = datetime.date (2001, 6, 3) #The day I moved to California the first time
+GA = datetime.date (2005, 9, 20) #The day I moved back to Georgia
+NY = datetime.date(2013, 8, 25) #The day I moved to New York
+CA2= datetime.date(2017, 7, 7) #The day I moved to California the second time
+print ("I moved to California the first time on {:%A, %B %d, %Y}.")
